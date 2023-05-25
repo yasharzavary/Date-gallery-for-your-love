c8b688268289bd24bdbce78043f6c27a7644aae1
yasharzavary <zavaryyashar009@gmail.com>
2023-05-25 21:59:40 +0430

    submit come in come out func finish

--- main.py
+++ main.py
@@ -27,8 +27,22 @@ passLabel.pack()
 passEntry=Entry(master=root)
 passEntry.pack()
 
+def nameAndPassControl(event):
+    pass
+
+def comein(event):
+    nameButton.config(bg='#888888')
+
+def comeout(event):
+    nameButton.config(bg='#F0F0F0')
+
+
+
 # submit button part
-nameButton=Button(master=root, text='submit')
+nameButton=Button(master=root, text='submit', bg='#F0F0F0')
+nameButton.bind('<Enter>',comein)
+nameButton.bind('<Leave>',comeout)
+nameButton.bind('<Button>',nameAndPassControl)
 nameButton.pack()
     
 
